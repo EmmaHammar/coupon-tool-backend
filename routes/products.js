6bb7f1167163702f3d21198a4d5d5e53d45449de
EmmaHammar <emma.s.hammar@gmail.com>
2022-03-25 14:06:52 +0100

    handle cors err

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors');
+
+router.use(cors());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
