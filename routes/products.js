5a742583d940f683b4d4c2cc70d11b1b484b7904
EmmaHammar <emma.s.hammar@gmail.com>
2022-03-25 15:00:38 +0100

    cleaning products router

--- routes/products.js
+++ routes/products.js
@@ -4,12 +4,11 @@ const cors = require('cors');
 
 router.use(cors());
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   req.app.locals.db.collection('products').find({}).toArray()
   .then(products => {
     console.log("products", products);
-    res.json({'products': products});
+    res.send(products);
   });
 });
 
